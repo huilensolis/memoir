e5dfd4a43932aff0ecd1e80844cc61a947f6d741
Huilensolis <xywvanz@gmail.com>
2024-02-17 18:58:07 -0300

    feat(app): 39 change auth layout design

--- apps/client/src/app/auth/layout.tsx
+++ apps/client/src/app/auth/layout.tsx
@@ -12,7 +12,7 @@ export default async function AuthLayout({
 }) {
   return (
     <WithUserNotAuthenticated redirectUrl={ClientRoutingService.app.home}>
-      <div className="grid md:grid-cols-2 grid-cols-1 grid-rows-1 w-full max-w-7xl h-screen p-5">
+      <div className="grid xl:grid-cols-2 grid-cols-1 grid-rows-1 w-full max-w-[110rem] h-screen p-5">
         <section className="flex flex-col items-center justify-center h-full w-full">
           <nav className="w-full flex justify-end items-center gap-2 px-5 py-2">
             <Link href="/auth/sign-up">SignUp</Link>
@@ -20,26 +20,43 @@ export default async function AuthLayout({
           </nav>
           {children}
         </section>
-        <section className="w-full h-full md:flex hidden relative">
-          <ul className="grid grid-cols-3 grid-rows-6 w-full">
-            <li className="col-span-2 w-full h-full border-b border-r border-neutral-300"></li>
-            <li className="w-full h-full"></li>
-            <li className="w-full h-full border-r border-neutral-300"></li>
-            <li className="col-span-2 w-full h-full"></li>
-            <li className="col-span-3 w-full h-full border-t border-neutral-300"></li>
-            <li className="col-span-2 w-full h-ful"></li>
-            <li className="w-full h-full"></li>
-            <li className="w-full h-full border-r border-neutral-300"></li>
-            <li className="col-span-2 w-full h-full border-t border-neutral-300"></li>
-            <li className="col-span-2 w-full h-full border-t border-r border-neutral-300"></li>
+        <section className="w-full h-full xl:flex hidden relative">
+          <ul className="grid grid-cols-6 grid-rows-8 w-full">
+            <li className="w-full h-full opacity-0"></li>
+            <li className="w-full h-full bg-neutral-300"></li>
+            <li className="w-full h-full border-t border-x border-neutral-300"></li>
+            <li className="w-full h-full opacity-0"></li>
+            <li className="w-full h-full border border-l border-t border-b border-neutral-300"></li>
+            <li className="w-full h-full bg-neutral-300"></li>
+            <li className="w-full h-full bg-neutral-300"></li>
+            <li className="w-full h-full border border-neutral-300 opacity-0"></li>
+            <li className="w-full h-full bg-neutral-300"></li>
+            <li className="w-full h-full border-y border-l border-neutral-300"></li>
+            <li className="w-full h-full bg-neutral-300"></li>
+            <li className="col-span-5 w-full h-full opacity-0"></li>
+            <li className="w-full h-full border border-neutral-300"></li>
+            <li className="w-full h-full col-span-6 row-span-2 flex items-center justify-start">
+              <h2 className="text-balance text-neutral-700 font-bold text-5xl max-w-2xl w-full">
+                Build a journaling habit and document your dreams, reflections,
+                experiences, mood and memories.
+              </h2>
+            </li>
+            <li className="col-span-5 w-full h-full"></li>
+            <li className="w-full h-full border-x border-t border-neutral-300"></li>
+            <li className="w-full h-full opacity-0"></li>
+            <li className="w-full h-full bg-neutral-300"></li>
+            <li className="w-full h-full opacity-0"></li>
+            <li className="w-full h-full border-y border-neutral-300 opacity-0"></li>
+            <li className="w-full h-full border border-neutral-300"></li>
+            <li className="w-full h-full bg-neutral-300"></li>
+            <li className="w-full h-full border border-neutral-300"></li>
+            <li className="w-full h-full opacity-0"></li>
+            <li className="w-full h-full bg-neutral-300"></li>
+            <li className="w-full h-full border border-neutral-300"></li>
+            <li className="w-full h-full bg-neutral-300"></li>
             <li className="w-full h-full"></li>
           </ul>
-          <div className="absolute top-0 left-0 h-full w-full flex items-center justify-center">
-            <h2 className="text-balance text-center text-neutral-700 font-bold text-5xl">
-              Begin your journey of self-discovery – one entry a day. Unlock the
-              power of reflection and growth with Memoir
-            </h2>
-          </div>
+          <div className="absolute top-0 left-0 h-full w-full flex items-center justify-center"></div>
         </section>
       </div>
     </WithUserNotAuthenticated>
