d2620d2f397b2db7509c5ba9edf898a4e452752e
Huilensolis <xywvanz@gmail.com>
2024-02-17 18:09:18 -0300

    docs(app): 39 refactor auth pages texts

--- apps/client/src/app/auth/sign-in/page.tsx
+++ apps/client/src/app/auth/sign-in/page.tsx
@@ -2,14 +2,10 @@ import { SignInForm } from "./components/form";
 
 export default function SignInPage() {
   return (
-    <article className="w-full max-w-md h-full flex flex-col gap-2 items-center justify-center">
-      <h1 className="text-4xl text-center w-full font-extrabold text-balance">
-        Sign In
+    <article className="w-full max-w-md h-full flex flex-col gap-6 items-center justify-center">
+      <h1 className="text-4xl w-full font-medium text-neutral-700 text-balance">
+        Log In to Memoir
       </h1>
-
-      <h2 className="text-2xl text-center w-full font-semibold text-balance">
-        Its nice to see you back!
-      </h2>
       <section className="w-full">
         <SignInForm />
       </section>
--- apps/client/src/app/auth/sign-up/page.tsx
+++ apps/client/src/app/auth/sign-up/page.tsx
@@ -3,14 +3,9 @@ import { SignUpForm } from "./components/form/";
 export default function SignUpPage() {
   return (
     <article className="w-full max-w-md h-full flex flex-col gap-6 items-center justify-center">
-      <section>
-        <h1 className="text-4xl text-center w-full font-extrabold text-balance">
-          Memoir
-        </h1>
-        <h2 className="text-2xl text-center w-full font-semibold text-balance">
-          Make journaling a habit, enhance mental clarity, know thyself.
-        </h2>
-      </section>
+      <h1 className="text-4xl w-full font-medium text-neutral-700 text-balance">
+        Create an account on Memoir
+      </h1>
       <section className="w-full">
         <SignUpForm />
       </section>
