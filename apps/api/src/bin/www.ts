e96f5e0676c7f074213ba8c46ae3997361d36b75
Huilensolis <xywvanz@gmail.com>
2024-03-03 21:31:19 -0300

    feat(server): 75 add limit rate

--- apps/api/src/bin/www.ts
+++ apps/api/src/bin/www.ts
@@ -4,5 +4,14 @@ import { app } from "../app";
 (() => {
   const port = Environment.PORT;
   app.listen(port);
+
   console.log(`app running on port ${port}`);
+
+  if (Environment.NODE_ENV) {
+    console.log(`running in ${Environment.NODE_ENV} mode`);
+  } else {
+    console.log(
+      "no environment provided on .env file, make sure to set the NODE_ENV variable",
+    );
+  }
 })();
