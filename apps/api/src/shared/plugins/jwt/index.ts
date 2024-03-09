aeab757d7841a9310a3c64c56a5bd4a751e69961
Huilensolis <xywvanz@gmail.com>
2024-03-09 14:37:17 -0300

    refactor(client): 82 configure samesite and strict on cookies

--- apps/api/src/shared/plugins/jwt/index.ts
+++ apps/api/src/shared/plugins/jwt/index.ts
@@ -4,32 +4,35 @@ import cookie from "@elysiajs/cookie";
 import { Environment } from "@/config/environment";
 
 export const JwtPlugin = new Elysia()
-	.use(
-		jwt({
-			name: "jwt",
-			secret: Environment.JWT_SECRET,
-			schema: t.Object({
-				user: t.Object({
-					id: t.String(),
-					name: t.String(),
-					email: t.String(),
-				}),
-			}),
-			exp: new Date().getTime() + 1000 * 60 * 60 * 24 * 14, // 14 days in the future
-		}),
-	)
-	.use(
-		cookie({
-			httpOnly: true,
-			expires: new Date(
-				new Date().getTime() +
-					1000 /* 1 second */ *
-						60 /* 1 minute */ *
-						60 /* 1 hour */ *
-						24 /* 1 day */ *
-						14 /*14 days */,
-			),
-			sameSite: "none",
-			secure: true,
-		}),
-	);
+  .use(
+    jwt({
+      name: "jwt",
+      secret: Environment.JWT_SECRET,
+      schema: t.Object({
+        user: t.Object({
+          id: t.String(),
+          name: t.String(),
+          email: t.String(),
+        }),
+      }),
+      exp: new Date().getTime() + 1000 * 60 * 60 * 24 * 14, // 14 days in the future
+    }),
+  )
+  .use(
+    cookie({
+      httpOnly: true,
+      expires: new Date(
+        new Date().getTime() +
+          1000 /* 1 second */ *
+            60 /* 1 minute */ *
+            60 /* 1 hour */ *
+            24 /* 1 day */ *
+            14 /*14 days */,
+      ),
+      sameSite: "strict",
+      secure: true,
+      ...(Environment.NODE_ENV === "production" && {
+        domain: Environment.WEB_DOMAIN,
+      }),
+    }),
+  );
