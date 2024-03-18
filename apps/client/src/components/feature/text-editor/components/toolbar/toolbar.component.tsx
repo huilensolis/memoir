efa31aa05f70777a3bafdbf0ffb17e9660f98ee0
Huilensolis <xywvanz@gmail.com>
2024-03-18 00:39:09 -0300

    feat(client): 88 add floating menu and toolbar to text editor

--- apps/client/src/components/feature/text-editor/components/toolbar/toolbar.component.tsx
+++ apps/client/src/components/feature/text-editor/components/toolbar/toolbar.component.tsx
@@ -1,7 +1,23 @@
 import { type Editor } from "@tiptap/react";
-import { Bold, Italic, Strikethrough } from "lucide-react";
+import {
+  Bold,
+  Heading1,
+  Heading2,
+  Heading3,
+  Italic,
+  Redo,
+  Strikethrough,
+  Undo,
+} from "lucide-react";
 
-import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+} from "@/components/ui/command";
 
 type TProps = {
   editor: Editor;
@@ -13,6 +29,7 @@ export function Toolbar({ editor }: TProps) {
   }
 
   function convertToBold() {
+    console.log("running");
     editor.chain().focus().toggleBold().run();
   }
 
@@ -20,20 +37,116 @@ export function Toolbar({ editor }: TProps) {
     editor.chain().focus().toggleStrike().run();
   }
 
+  function undo() {
+    editor.chain().undo().run();
+  }
+
+  function redo() {
+    editor.chain().redo().run();
+  }
+
+  function convertToQuote() {}
+
+  function toggleHeading({ level }: { level: number }) {
+    editor.commands.insertContent({
+      type: "heading",
+      attrs: {
+        level,
+      },
+      content: [
+        {
+          type: "text",
+          text: "Title",
+        },
+      ],
+    });
+
+    editor.chain().focus().run();
+  }
+
   return (
-    <div className="flex items-center space-x-2">
-      <Button size="sm" variant="outline" onClick={convertToBold}>
-        <Bold className="h-4 w-4" />
-        <span className="sr-only">Bold</span>
-      </Button>
-      <Button size="sm" variant="outline" onClick={convertToItalic}>
-        <Italic className="h-4 w-4" />
-        <span className="sr-only">Italic</span>
-      </Button>
-      <Button size="sm" variant="outline" onClick={convertToStrike}>
-        <Strikethrough className="h-4 w-4" />
-        <span className="sr-only">Strike</span>
-      </Button>
-    </div>
+    <Command className="rounded-lg border border-gray-200 shadow-md dark:border-gray-800">
+      <CommandInput placeholder="Type a command or search..." />
+      <CommandList>
+        <CommandGroup heading="Text Formatting">
+          <CommandItem>
+            <button className="w-full flex" onClick={convertToBold}>
+              <Bold className="mr-2 h-4 w-4" />
+              <span>Bold</span>
+            </button>
+          </CommandItem>
+          <CommandItem>
+            <button className="w-full flex" onClick={convertToItalic}>
+              <Italic className="mr-2 h-4 w-4" />
+              <span>Italic</span>
+            </button>
+          </CommandItem>
+          <CommandItem>
+            <button className="w-full flex" onClick={convertToStrike}>
+              <Strikethrough className="mr-2 h-4 w-4" />
+              <span>Strike</span>
+            </button>
+          </CommandItem>
+        </CommandGroup>
+        <CommandSeparator />
+        <CommandGroup heading="Blocks">
+          <CommandItem value="Heading-1">
+            <button
+              className="w-full flex"
+              onClick={() => {
+                toggleHeading({ level: 1 });
+              }}
+            >
+              <Heading1 className="mr-2 h-4 w-4" />
+              <span>Heading</span>
+            </button>
+          </CommandItem>
+          <CommandItem value="heading-2">
+            <button
+              className="w-full flex"
+              onClick={() => {
+                toggleHeading({ level: 2 });
+              }}
+            >
+              <Heading2 className="mr-2 h-4 w-4" />
+              <span>Heading</span>
+            </button>
+          </CommandItem>
+          <CommandItem value="heading-3">
+            <button
+              className="w-full flex"
+              onClick={() => {
+                toggleHeading({ level: 3 });
+              }}
+            >
+              <Heading3 className="mr-2 h-4 w-4" />
+              <span>Heading</span>
+            </button>
+          </CommandItem>
+        </CommandGroup>
+        <CommandSeparator />
+        <CommandGroup heading="Actions">
+          <CommandItem disabled={!editor.can().undo()}>
+            <button className="w-full flex" onClick={undo}>
+              <Undo className="mr-2 h-4 w-4" />
+              <span>Undo</span>
+            </button>
+          </CommandItem>
+          <CommandItem disabled={!editor.can().redo()}>
+            <button className="w-full flex" onClick={redo}>
+              <Redo className="mr-2 h-4 w-4" />
+              <span>Redo</span>
+            </button>
+          </CommandItem>
+          <CommandItem>
+            <button className="w-full flex" onClick={convertToQuote}>
+              <Undo className="mr-2 h-4 w-4" />
+              <span>Convert to quote</span>
+            </button>
+          </CommandItem>
+        </CommandGroup>
+        <CommandSeparator />
+      </CommandList>
+    </Command>
   );
 }
--- apps/client/src/components/feature/text-editor/text-editor.component.tsx
+++ apps/client/src/components/feature/text-editor/text-editor.component.tsx
@@ -1,15 +1,31 @@
 "use client";
 
-import { useEditor, EditorContent, BubbleMenu } from "@tiptap/react";
+import {
+  useEditor,
+  EditorContent,
+  BubbleMenu,
+  FloatingMenu,
+} from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import Typography from "@tiptap/extension-typography";
+import Placeholder from "@tiptap/extension-placeholder";
+
 import { Toolbar } from "./components/toolbar";
 
 export function TextEditor() {
   const editor = useEditor({
+    editorProps: {
+      attributes: {
+        class: "outline-none py-4",
+      },
+    },
     extensions: [
       StarterKit.configure({
         heading: {
           levels: [1, 2, 3],
+          HTMLAttributes: {
+            class: "font-bold",
+          },
         },
         bold: {
           HTMLAttributes: {
@@ -26,9 +42,27 @@ export function TextEditor() {
             class: "line-through",
           },
         },
+        code: false,
+      }),
+      Typography.configure({
+        oneHalf: false,
+        oneQuarter: false,
+        threeQuarters: false,
+        laquo: false,
+        raquo: false,
+      }),
+      Placeholder.configure({
+        placeholder: ({ node }) => {
+          if (node.type.name === "heading") return "Title";
+          return "";
+        },
+        emptyEditorClass:
+          "before:[content:_attr(data-placeholder);] before:outline-none before:h-0 before:pointer-events-none before:float-left before:text-neutral-400",
       }),
     ],
-    content: "Hello World!",
+    autofocus: true,
+    editable: true,
+    injectCSS: false,
   });
 
   return (
@@ -38,7 +72,13 @@ export function TextEditor() {
           <BubbleMenu editor={editor} updateDelay={300}>
             <Toolbar editor={editor} />
           </BubbleMenu>
-          <EditorContent editor={editor} className="w-full h-full" />
+          <FloatingMenu editor={editor} tippyOptions={{ duration: 100 }}>
+            <Toolbar editor={editor} />
+          </FloatingMenu>
+          <EditorContent
+            editor={editor}
+            className="w-full h-full min-h-screen"
+          />
         </>
       )}
     </div>
