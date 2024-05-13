ee3f2febe323ffbe7852b816c8e23e897eee3751
Huilensolis <xywvanz@gmail.com>
2024-05-13 12:56:13 -0300

    refactor: auth layout ui

--- packages/client/src/app/(landing)/page.tsx
+++ packages/client/src/app/(landing)/page.tsx
@@ -6,28 +6,30 @@ import Link from "next/link";
 export default function Home() {
   return (
     <WithUserNotAuthenticated redirectUrl={ClientRoutingService.app.home}>
-      <nav className="w-full items-center justify-center flex gap-1">
-        <Link
-          href={ClientRoutingService.auth.signUp}
-          className="flex items-center p-1 px-3 hover:bg-neutral-200 transition-all duration-75 rounded-sm font-semibold text-neutral-700"
-        >
-          Sign Up
-          <ArrowUpRight className="h-4 w-4" />
-        </Link>
-        <Link
-          href={ClientRoutingService.auth.signIn}
-          className="flex items-center p-1 px-3 hover:bg-neutral-200 transition-all duration-75 rounded-sm font-semibold text-neutral-700"
-        >
-          Sign In
-          <ArrowUpRight className="h-4 w-4" />
-        </Link>
-      </nav>
-      <article className="flex flex-col w-full items-center justify-center py-80">
-        <h1 className="text-balance text-center text-neutral-700 font-bold text-5xl max-w-2xl w-full">
-          Build a journaling habit and document your dreams, reflections,
-          experiences, mood and memories.
-        </h1>
-      </article>
+      <div className="w-full max-w-2xl">
+        <nav className="w-full items-center justify-end flex gap-1">
+          <Link
+            href={ClientRoutingService.auth.signUp}
+            className="flex items-center p-1 px-3 hover:bg-neutral-200 transition-all duration-75 rounded-sm font-semibold text-neutral-700"
+          >
+            Sign Up
+            <ArrowUpRight className="h-4 w-4" />
+          </Link>
+          <Link
+            href={ClientRoutingService.auth.signIn}
+            className="flex items-center p-1 px-3 hover:bg-neutral-200 transition-all duration-75 rounded-sm font-semibold text-neutral-700"
+          >
+            Sign In
+            <ArrowUpRight className="h-4 w-4" />
+          </Link>
+        </nav>
+        <article className="flex flex-col w-full items-center justify-center py-80">
+          <h1 className="text-balance text-center text-neutral-700 font-bold text-5xl max-w-2xl w-full">
+            Build a journaling habit and document your dreams, reflections,
+            experiences, mood and memories.
+          </h1>
+        </article>
+      </div>
     </WithUserNotAuthenticated>
   );
 }
--- packages/client/src/app/auth/layout.tsx
+++ packages/client/src/app/auth/layout.tsx
@@ -11,46 +11,15 @@ export default async function AuthLayout({
 }) {
   return (
     <WithUserNotAuthenticated redirectUrl={ClientRoutingService.app.home}>
-      <div className="grid xl:grid-cols-2 grid-cols-1 grid-rows-1 w-full max-w-[110rem] max-h-[60rem] h-screen">
-        <section className="flex flex-col items-center justify-center h-full w-full xl:border-r xl:border-neutral-300 py-5 px-5">
+      <div className="grid xl:grid-cols-[1fr_2fr] grid-cols-1 grid-rows-1 items-center justify-center w-full h-screen">
+        <section className="flex flex-col items-center justify-center h-full w-full py-5 px-5">
           {children}
         </section>
-        <section className="w-full h-full xl:flex hidden relative max-h-screen">
-          <ul className="grid grid-cols-6 grid-rows-8 w-full">
-            <li className="w-full h-full opacity-0"></li>
-            <li className="w-full h-full bg-neutral-300"></li>
-            <li className="w-full h-full border-t border-x border-neutral-300"></li>
-            <li className="w-full h-full opacity-0"></li>
-            <li className="w-full h-full border border-l border-t border-b border-neutral-300"></li>
-            <li className="w-full h-full bg-neutral-300"></li>
-            <li className="w-full h-full bg-neutral-300"></li>
-            <li className="w-full h-full border border-neutral-300 opacity-0"></li>
-            <li className="w-full h-full bg-neutral-300"></li>
-            <li className="w-full h-full border-y border-l border-neutral-300"></li>
-            <li className="w-full h-full bg-neutral-300"></li>
-            <li className="col-span-5 w-full h-full opacity-0"></li>
-            <li className="w-full h-full border border-neutral-300"></li>
-            <li className="w-full h-full col-span-6 row-span-2 flex items-center justify-center">
-              <h2 className="text-balance text-neutral-700 font-bold text-5xl max-w-2xl w-full">
-                Build a journaling habit and document your dreams, reflections,
-                experiences, mood and memories.
-              </h2>
-            </li>
-            <li className="col-span-5 w-full h-full"></li>
-            <li className="w-full h-full border-x border-t border-neutral-300"></li>
-            <li className="w-full h-full opacity-0"></li>
-            <li className="w-full h-full bg-neutral-300"></li>
-            <li className="w-full h-full opacity-0"></li>
-            <li className="w-full h-full border-y border-neutral-300 opacity-0"></li>
-            <li className="w-full h-full border border-neutral-300"></li>
-            <li className="w-full h-full bg-neutral-300"></li>
-            <li className="w-full h-full border-t border-r border-b border-neutral-300"></li>
-            <li className="w-full h-full opacity-0"></li>
-            <li className="w-full h-full bg-neutral-300"></li>
-            <li className="w-full h-full border border-neutral-300"></li>
-            <li className="w-full h-full bg-neutral-300"></li>
-            <li className="w-full h-full"></li>
-          </ul>
+        <section className="w-full h-full xl:flex xl:items-center xl:justify-center hidden relative max-h-screen bg-neutral-900">
+          <h2 className="text-balance text-neutral-200 font-bold text-5xl max-w-2xl w-full">
+            Build a journaling habit and document your dreams, reflections,
+            experiences, mood and memories.
+          </h2>
         </section>
       </div>
     </WithUserNotAuthenticated>
