162c3cb32871ea91808b350b15a814379bf1b805
Huilensolis <xywvanz@gmail.com>
2024-05-25 16:13:40 -0300

    feat(client): add close btn to search entry command modal

--- apps/client/src/components/feature/entry-search-modal/entry-search-modal.component.tsx
+++ apps/client/src/components/feature/entry-search-modal/entry-search-modal.component.tsx
@@ -10,6 +10,8 @@ import {
 } from "@/components/ui/command";
 import { EntryList } from "./entry-list";
 import { createPortal } from "react-dom";
+import { Button } from "@/components/ui/button";
+import { X } from "lucide-react";
 
 export function EntrySearchModalProvider() {
   const showModal = useSearchEntryModalStore((state) => state.showModal);
@@ -22,11 +24,16 @@ export function EntrySearchModalProvider() {
       {createPortal(
         <div className="fixed top-0 left-0 w-full h-screen flex items-center justify-center bg-neutral-900/20 z-50 overflow-y-hidden">
           <Command className="rounded-lg border border-gray-200 shadow-md dark:border-gray-800 max-w-2xl max-h-96">
-            <CommandInput
-              placeholder="Type an entry title to search for matches..."
-              autoFocus
-              className="text-base"
-            />
+            <div className="w-full flex items-center justify-between">
+              <CommandInput
+                placeholder="Type an entry title to search for matches..."
+                autoFocus
+                className="text-base w-full"
+              />
+              <Button variant="ghost" size="sm" onClick={toggleModal}>
+                <X />
+              </Button>
+            </div>
             <CommandList>
               <CommandSeparator />
               <CommandGroup
