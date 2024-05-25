ab9082bfe4ea4563e9a7342f915eef14cd92b1d0
Huilensolis <xywvanz@gmail.com>
2024-05-25 17:35:50 -0300

    feat(client): add focus trap to search entry modal

--- apps/client/src/components/feature/entry-search-modal/entry-search-modal.component.tsx
+++ apps/client/src/components/feature/entry-search-modal/entry-search-modal.component.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useEffect } from "react";
+import FocusTrap from "focus-trap-react";
 import { useSearchEntryModalStore } from "@/app/app/(stores)/search-entry-command-modal";
 import {
   Command,
@@ -12,7 +14,6 @@ import { EntryList } from "./entry-list";
 import { createPortal } from "react-dom";
 import { Button } from "@/components/ui/button";
 import { X } from "lucide-react";
-import { useEffect } from "react";
 
 export function EntrySearchModalProvider() {
   const showModal = useSearchEntryModalStore((state) => state.showModal);
@@ -40,43 +41,46 @@ export function EntrySearchModalProvider() {
   return (
     <>
       {createPortal(
-        <div
-          className="fixed top-0 left-0 w-full h-screen flex items-center justify-center bg-neutral-900/20 z-50 overflow-y-hidden"
-          onClick={toggleModal}
-        >
-          <Command
-            className="rounded-lg border border-gray-200 shadow-md dark:border-gray-800 max-w-2xl max-h-96"
-            onClick={(e) => {
-              e.stopPropagation();
-            }}
+        <FocusTrap>
+          <div
+            className="fixed top-0 left-0 w-full h-screen flex items-center justify-center bg-neutral-900/20 z-50 overflow-y-hidden"
+            onClick={toggleModal}
           >
-            <div className="w-full flex items-center justify-between">
-              <CommandInput
-                placeholder="Type an entry title to search for matches..."
-                autoFocus
-                className="text-base w-full"
-              />
-              <Button
-                variant="ghost"
-                size="sm"
-                onClick={toggleModal}
-                className="text-muted-foreground"
-              >
-                <X />
-              </Button>
-            </div>
-            <CommandList>
-              <CommandSeparator />
-              <CommandGroup
-                heading="Entries"
-                className="[&_[cmdk-group-heading]]:text-sm"
-              >
-                <EntryList />
-              </CommandGroup>
-              <CommandSeparator />
-            </CommandList>
-          </Command>
-        </div>,
+            <Command
+              className="rounded-lg border border-gray-200 shadow-md dark:border-gray-800 max-w-2xl max-h-96"
+              onClick={(e) => {
+                e.stopPropagation();
+              }}
+              role="dialog"
+            >
+              <div className="w-full flex items-center justify-between">
+                <CommandInput
+                  placeholder="Type an entry title to search for matches..."
+                  autoFocus
+                  className="text-base w-full"
+                />
+                <Button
+                  variant="ghost"
+                  size="sm"
+                  onClick={toggleModal}
+                  className="text-muted-foreground"
+                >
+                  <X />
+                </Button>
+              </div>
+              <CommandList>
+                <CommandSeparator />
+                <CommandGroup
+                  heading="Entries"
+                  className="[&_[cmdk-group-heading]]:text-sm"
+                >
+                  <EntryList />
+                </CommandGroup>
+                <CommandSeparator />
+              </CommandList>
+            </Command>
+          </div>
+        </FocusTrap>,
         document.body,
       )}
     </>
