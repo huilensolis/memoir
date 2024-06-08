7a33aef416ff46e2cfe6999d6b153bb160bfb8be
Huilensolis <xywvanz@gmail.com>
2024-06-08 14:00:55 -0300

    fix duplicated layout error

--- apps/client/src/components/feature/server-status/index.ts
+++ apps/client/src/components/feature/server-status/index.ts
@@ -1,6 +1,1 @@
-import dynamic from "next/dynamic";
-
-// eslint-diable-next-line
-export const ServerStatus = dynamic(() => import("./server-status.component"), {
-  ssr: false,
-});
+export * from "./server-status.component";
--- apps/client/src/components/feature/server-status/server-status.component.tsx
+++ apps/client/src/components/feature/server-status/server-status.component.tsx
@@ -8,55 +8,60 @@ import { Spinner } from "@/components/ui/spinner";
 import { ApiRoutingService } from "@/models/routing/api";
 import { BadgeCheck } from "lucide-react";
 
-export default function ServerStatus() {
-  useEffect(() => {
-    toast.promise(
-      async () => {
-        // eslint-disable-next-line
-        return new Promise(async (resolve, _reject) => {
-          let isServerDown = true;
-          while (isServerDown) {
-            try {
-              const { status } = await axios.get(
-                ApiRoutingService.routing.health,
-                {
-                  timeout: 3000,
-                },
-              );
-
-              if (status !== 200) throw new Error("Unhealthy server");
-
-              isServerDown = false;
-              // eslint-disable-next-line @typescript-eslint/no-confusing-void-expression
-              return resolve("");
-            } catch (error) {}
-          }
+function checkStatus() {
+  // eslint-disable-next-line
+  return new Promise(async (resolve, _reject) => {
+    if (typeof document === "undefined") return resolve("");
+    let isServerDown = true;
+    while (isServerDown) {
+      await new Promise((resolve) =>
+        setTimeout(() => {
+          resolve("");
+        }, 2000),
+      );
+      try {
+        const { status } = await axios.get(ApiRoutingService.routing.health, {
+          timeout: 3000,
         });
-      },
-      {
-        loading: (
-          <article className="flex flex-col">
-            <header className="flex items-center gap-2">
-              <Spinner />
-              <strong>Server is down</strong>
-            </header>
-            <p>
-              The server is hosted on a free plan, which means that it turns off
-              when it has no activity. <br /> <br /> it can take up to 50s to
-              turn on the server.
-            </p>
-          </article>
-        ),
-        success: (
-          <article className="flex flex-col">
-            <header className="flex items-center gap-2">
-              <BadgeCheck className="w-4 h-4 text-neutral-950" />
-              <strong>Server is up</strong>
-            </header>
-          </article>
-        ),
-      },
-    );
+
+        if (status !== 200) throw new Error("Unhealthy server");
+
+        isServerDown = false;
+        resolve("");
+      } catch (error) {}
+    }
+  });
+}
+
+export function ServerStatus() {
+  function showToaster() {
+    toast.promise(checkStatus, {
+      loading: (
+        <article className="flex flex-col">
+          <header className="flex items-center gap-2">
+            <Spinner />
+            <strong>Server is down</strong>
+          </header>
+          <p>
+            The server is hosted on a free plan, which means that it turns off
+            when it has no activity. <br /> <br /> it can take up to 50s to turn
+            on the server.
+          </p>
+        </article>
+      ),
+      success: (
+        <article className="flex flex-col">
+          <header className="flex items-center gap-2">
+            <BadgeCheck className="w-4 h-4 text-neutral-950" />
+            <strong>Server is up</strong>
+          </header>
+        </article>
+      ),
+    });
+  }
+
+  useEffect(() => {
+    showToaster();
   }, []);
 
   return null;
