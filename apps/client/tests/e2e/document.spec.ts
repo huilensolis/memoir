c091638a7376e82a4cb662a33954b08769425fb4
Huilensolis <xywvanz@gmail.com>
2024-06-12 19:13:24 -0300

    test(client): add tests for searching for an entry and reading it

--- apps/client/tests/e2e/document.spec.ts
+++ apps/client/tests/e2e/document.spec.ts
@@ -24,7 +24,36 @@ test.describe("Document", () => {
     );
   });
 
-  test("can read document", () => {});
+  test("can read document from search-entry command modal - opened from aside button", async ({
+    page,
+  }) => {
+    await page.goto("http://localhost:3000/app");
+
+    await page.getByRole("button", { name: "Search Entry" }).click();
+
+    await page.getByRole("option", { name: "Untitled" }).first().click();
+
+    await expect(page).toHaveURL(
+      new RegExp("^http://localhost:3000/app/entry/"),
+    );
+  });
+
+  test("can read document from search-entry command modal - opened from home menu", async ({
+    page,
+  }) => {
+    await page.goto("http://localhost:3000/app");
+
+    await page
+      .locator("button")
+      .filter({ hasText: "Search EntrySearch and find" })
+      .click();
+
+    await page.getByRole("option", { name: "Untitled" }).first().click();
+
+    await expect(page).toHaveURL(
+      new RegExp("^http://localhost:3000/app/entry/"),
+    );
+  });
 
   test("can edit title of document", () => {});
 
