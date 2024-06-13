e7533ee090ac6f2d152fda69eaae7a587d579a7f
Huilensolis <xywvanz@gmail.com>
2024-06-13 14:41:26 -0300

    fix(client): make landing page accesible to authenticated users

---
+++ apps/client/src/app/(landing)/auth-btns/index.tsx
@@ -0,0 +1,61 @@
+import { AuthService } from "@/models/api/auth";
+import { ClientRoutingService } from "@/models/routing/client";
+import { getCookie } from "@/utils/getCookies";
+import { ArrowUpRight, Satellite } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+function AuthLinks() {
+  return (
+    <div className="flex gap-2 items-center">
+      <Link
+        href={ClientRoutingService.auth.signUp}
+        className="flex items-center gap-1 p-1 px-3 hover:bg-neutral-200 transition-all duration-75 rounded-sm font-semibold text-neutral-700"
+      >
+        Sign Up
+        <ArrowUpRight className="h-4 w-4" />
+      </Link>
+      <Link
+        href={ClientRoutingService.auth.signIn}
+        className="flex items-center gap-1 p-1 px-3 hover:bg-neutral-200 transition-all duration-75 rounded-sm font-semibold text-neutral-700"
+      >
+        Sign In
+        <ArrowUpRight className="h-4 w-4" />
+      </Link>
+    </div>
+  );
+}
+
+function AppLink() {
+  return (
+    <Link
+      href={ClientRoutingService.app.home}
+      className="flex items-center gap-1 p-1 px-3 hover:bg-neutral-200 transition-all duration-75 rounded-sm font-semibold text-neutral-700"
+    >
+      Launch App
+      <Satellite className="h-4 w-4" />
+    </Link>
+  );
+}
+
+const VIEWS: Record<string, React.JSX.Element> = {
+  authenticated: <AuthLinks />,
+  "non-authenticated": <AppLink />,
+};
+
+export async function AuthBtns() {
+  const { cookie } = getCookie();
+
+  if (!cookie) {
+    return VIEWS["non-authenticated"];
+  }
+
+  await new Promise((resolve) => setTimeout(() => resolve(""), 4000));
+  const { user } = await AuthService.getUser({ Cookie: cookie });
+
+  if (!user) {
+    return VIEWS["non-authenticated"];
+  }
+
+  return VIEWS["authenticated"];
+}
--- apps/client/src/app/(landing)/page.tsx
+++ apps/client/src/app/(landing)/page.tsx
@@ -1,35 +1,28 @@
-import { WithUserNotAuthenticated } from "@/components/wrappers/protect-from-authenticated-users";
-import { ClientRoutingService } from "@/models/routing/client";
-import { ArrowUpRight } from "lucide-react";
-import Link from "next/link";
+import { Suspense } from "react";
+import { AuthBtns } from "./auth-btns";
+import { Spinner } from "@/components/ui/spinner";
 
 export default function Home() {
   return (
-    <WithUserNotAuthenticated redirectUrl={ClientRoutingService.app.home}>
-      <div className="w-full max-w-2xl">
-        <nav className="w-full items-center justify-end flex gap-1">
-          <Link
-            href={ClientRoutingService.auth.signUp}
-            className="flex items-center p-1 px-3 hover:bg-neutral-200 transition-all duration-75 rounded-sm font-semibold text-neutral-700"
-          >
-            Sign Up
-            <ArrowUpRight className="h-4 w-4" />
-          </Link>
-          <Link
-            href={ClientRoutingService.auth.signIn}
-            className="flex items-center p-1 px-3 hover:bg-neutral-200 transition-all duration-75 rounded-sm font-semibold text-neutral-700"
-          >
-            Sign In
-            <ArrowUpRight className="h-4 w-4" />
-          </Link>
-        </nav>
-        <article className="flex flex-col w-full items-center justify-center py-80">
-          <h1 className="text-balance text-center text-neutral-700 font-bold text-5xl max-w-2xl w-full">
-            Build a journaling habit and document your dreams, reflections,
-            experiences, mood and memories.
-          </h1>
-        </article>
-      </div>
-    </WithUserNotAuthenticated>
+    <div className="w-full max-w-2xl">
+      <nav className="w-full items-center justify-end flex gap-2">
+        <Suspense
+          fallback={
+            <div className="flex items-center gap-1 p-1 px-3 hover:bg-neutral-200 transition-all duration-75 rounded-sm font-semibold text-neutral-700">
+              Authenticating
+              <Spinner className="w-4 h-4" />
+            </div>
+          }
+        >
+          <AuthBtns />
+        </Suspense>
+      </nav>
+      <article className="flex flex-col w-full items-center justify-center py-80">
+        <h1 className="text-balance text-center text-neutral-700 font-bold text-5xl max-w-2xl w-full">
+          Build a journaling habit and document your dreams, reflections,
+          experiences, mood and memories.
+        </h1>
+      </article>
+    </div>
   );
 }
