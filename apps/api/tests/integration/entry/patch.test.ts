7c16bcf171c1dbb822e9ba2d19fa792c30f8dee4
Huilensolis <xywvanz@gmail.com>
2024-06-24 14:12:28 -0300

    fix(client): tests

--- apps/api/tests/integration/entry/patch.test.ts
+++ apps/api/tests/integration/entry/patch.test.ts
@@ -6,37 +6,37 @@ import { createUser } from "@/tests/lib/user";
 import { endpointPath } from ".";
 
 describe("Test PATCH method on entries endpoints", () => {
-	describe("Succesfull request", async () => {
-		const { cookie } = await createUser({});
+  describe("Succesfull request", async () => {
+    const { cookie } = await createUser({});
 
-		const { EntryId } = await createNewEntry(
-			{ title: "untitled", content: EXAMPLE_DOCUMENT_CONTENT, word_count: 0 },
-			cookie,
-		);
+    const { EntryId } = await createNewEntry(
+      { title: "untitled", content: EXAMPLE_DOCUMENT_CONTENT, word_count: 0 },
+      cookie,
+    );
 
-		const res = await app.handle(
-			new Request(`${endpointPath}/${EntryId}`, {
-				method: "PATCH",
-				headers: {
-					"Content-Type": "application/json; charset=utf-8",
-					cookie: cookie,
-				},
-				body: JSON.stringify({
-					title: "test",
-					content: EXAMPLE_DOCUMENT_CONTENT,
-					word_count: 291,
-				}),
-			}),
-		);
+    const res = await app.handle(
+      new Request(`${endpointPath}/${EntryId}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+          cookie: cookie,
+        },
+        body: JSON.stringify({
+          title: "test",
+          content: EXAMPLE_DOCUMENT_CONTENT,
+          word_count: 291,
+        }),
+      }),
+    );
 
-		it("Should return status code 201", () => {
-			expect(res.status).toBe(201);
-		});
+    it("Should return status code 204", () => {
+      expect(res.status).toBe(204);
+    });
 
-		it("Should return empty object on body", async () => {
-			const body = await res.json();
+    it("Should return empty object on body", async () => {
+      const body = await res.json();
 
-			expect(body).toBeEmptyObject();
-		});
-	});
+      expect(body).toBeEmptyObject();
+    });
+  });
 });
