3bcd707aaab7c81d3dffd3cafb9f2248bf319772
Huilensolis <xywvanz@gmail.com>
2024-09-06 20:57:56 -0300

    feat(client): add missing files on last commit

---
+++ apps/client/src/components/feature/aside-nav/views/desktop/desktop.view.component.tsx
@@ -0,0 +1,15 @@
+import { Hr } from "@/components/ui/hr";
+import { type ReactNode } from "react";
+import { EntryList } from "../../components/entry-list";
+
+export function DesktopAsideView({ children }: { children: ReactNode }) {
+  return (
+    <>
+      <aside className="max-w-80 w-full h-full min-h-screen p-2 flex gap-2 flex-col items-center border-r border-neutral-300">
+        {children}
+        <Hr orientation="horizontal" className="my-1" />
+        <EntryList />
+      </aside>
+    </>
+  );
+}
