596709607aedb44074fafe6e39c2d7414a8a2d46
Github Action <huilensolis@users.noreply.github.com>
2024-09-11 18:13:52 +0000

    format(api): formatt code

--- apps/api/src/features/entry/providers/index.ts
+++ apps/api/src/features/entry/providers/index.ts
@@ -2,130 +2,130 @@ import { db } from "@/config/database";
 import type { TReturnHanler } from "@/shared/models/promises";
 import { and, eq, ilike, isNull } from "drizzle-orm";
 import type {
-  TInsertEntry,
-  TNewEntry,
-  TReadEntry,
+	TInsertEntry,
+	TNewEntry,
+	TReadEntry,
 } from "../models/entry.models";
 import { Entry } from "../schema";
 
 export class EntryProvider {
-  static async getPrivateEntryById({
-    entryId,
-    userId,
-  }: {
-    entryId: string;
-    userId: string;
-  }): Promise<TReadEntry | undefined> {
-    const [entry] = await db
-      .select()
-      .from(Entry)
-      .where(and(eq(Entry.id, entryId), eq(Entry.user_id, userId)));
+	static async getPrivateEntryById({
+		entryId,
+		userId,
+	}: {
+		entryId: string;
+		userId: string;
+	}): Promise<TReadEntry | undefined> {
+		const [entry] = await db
+			.select()
+			.from(Entry)
+			.where(and(eq(Entry.id, entryId), eq(Entry.user_id, userId)));
 
-    return entry;
-  }
+		return entry;
+	}
 
-  static async getPublicEntryById({
-    entryId,
-  }: {
-    entryId: string;
-  }): Promise<TReadEntry | undefined> {
-    const [entry] = await db.select().from(Entry).where(eq(Entry.id, entryId));
+	static async getPublicEntryById({
+		entryId,
+	}: {
+		entryId: string;
+	}): Promise<TReadEntry | undefined> {
+		const [entry] = await db.select().from(Entry).where(eq(Entry.id, entryId));
 
-    return entry;
-  }
+		return entry;
+	}
 
-  static async getEntriesListByUserId(userId: string): Promise<TReadEntry[]> {
-    const entries = await db
-      .select()
-      .from(Entry)
-      .where(eq(Entry.user_id, userId));
+	static async getEntriesListByUserId(userId: string): Promise<TReadEntry[]> {
+		const entries = await db
+			.select()
+			.from(Entry)
+			.where(eq(Entry.user_id, userId));
 
-    return entries;
-  }
+		return entries;
+	}
 
-  static async getPrivateEntryListByTitle({
-    title,
-    userId,
-  }: {
-    title: string;
-    userId: string;
-  }): Promise<TReadEntry[]> {
-    const entries = await db
-      .select()
-      .from(Entry)
-      .where(and(ilike(Entry.title, title), eq(Entry.user_id, userId)));
+	static async getPrivateEntryListByTitle({
+		title,
+		userId,
+	}: {
+		title: string;
+		userId: string;
+	}): Promise<TReadEntry[]> {
+		const entries = await db
+			.select()
+			.from(Entry)
+			.where(and(ilike(Entry.title, title), eq(Entry.user_id, userId)));
 
-    return entries;
-  }
+		return entries;
+	}
 
-  static async createEntry({
-    entry,
-    userId,
-  }: {
-    entry: TInsertEntry;
-    userId: TReadEntry["user_id"];
-  }): Promise<TReturnHanler<TReadEntry, string>> {
-    const newEntryValues: TNewEntry = {
-      content: {
-        type: "doc",
-        content: [
-          {
-            type: "paragraph",
-          },
-        ],
-      },
-      ...entry,
-      user_id: userId,
-    };
+	static async createEntry({
+		entry,
+		userId,
+	}: {
+		entry: TInsertEntry;
+		userId: TReadEntry["user_id"];
+	}): Promise<TReturnHanler<TReadEntry, string>> {
+		const newEntryValues: TNewEntry = {
+			content: {
+				type: "doc",
+				content: [
+					{
+						type: "paragraph",
+					},
+				],
+			},
+			...entry,
+			user_id: userId,
+		};
 
-    try {
-      const [newEntry] = await db
-        .insert(Entry)
-        .values(newEntryValues)
-        .returning();
+		try {
+			const [newEntry] = await db
+				.insert(Entry)
+				.values(newEntryValues)
+				.returning();
 
-      if (!newEntry) throw new Error("Failed to create entry");
+			if (!newEntry) throw new Error("Failed to create entry");
 
-      return { error: null, data: newEntry };
-    } catch (error) {
-      return { error: "error creating new entry", data: null };
-    }
-  }
+			return { error: null, data: newEntry };
+		} catch (error) {
+			return { error: "error creating new entry", data: null };
+		}
+	}
 
-  static async deleteEntry({
-    entryId,
-    userId,
-  }: {
-    entryId: string;
-    userId: string;
-  }): Promise<{ error: string | null }> {
-    try {
-      await db
-        .update(Entry)
-        .set({ end_date: new Date() })
-        .where(and(eq(Entry.id, entryId), eq(Entry.user_id, userId)));
-      return { error: null };
-    } catch (error) {
-      return { error: "unknown" };
-    }
-  }
+	static async deleteEntry({
+		entryId,
+		userId,
+	}: {
+		entryId: string;
+		userId: string;
+	}): Promise<{ error: string | null }> {
+		try {
+			await db
+				.update(Entry)
+				.set({ end_date: new Date() })
+				.where(and(eq(Entry.id, entryId), eq(Entry.user_id, userId)));
+			return { error: null };
+		} catch (error) {
+			return { error: "unknown" };
+		}
+	}
 
-  static async updateEntry({
-    values,
-    entryId,
-  }: {
-    entryId: TReadEntry["id"];
-    values: TInsertEntry;
-  }): Promise<{ error: string | null }> {
-    try {
-      await db
-        .update(Entry)
-        .set(values)
-        .where(and(eq(Entry.id, entryId), isNull(Entry.end_date)));
+	static async updateEntry({
+		values,
+		entryId,
+	}: {
+		entryId: TReadEntry["id"];
+		values: TInsertEntry;
+	}): Promise<{ error: string | null }> {
+		try {
+			await db
+				.update(Entry)
+				.set(values)
+				.where(and(eq(Entry.id, entryId), isNull(Entry.end_date)));
 
-      return { error: null };
-    } catch (error) {
-      return { error: "error updating entry" };
-    }
-  }
+			return { error: null };
+		} catch (error) {
+			return { error: "error updating entry" };
+		}
+	}
 }
