ce73d2389b209744ed172aff353fd479671a1a40
Github Action <huilensolis@users.noreply.github.com>
2024-10-09 23:35:03 +0000

    format(api): formatt code

--- apps/api/tests/integration/entry/post.test.ts
+++ apps/api/tests/integration/entry/post.test.ts
@@ -5,65 +5,65 @@ import { createUser } from "@/tests/lib/user";
 import { endpointPath } from ".";
 
 describe("Test POST method on entries endpoints", () => {
-    describe("Entry created succesfully", async () => {
-        const { cookie } = await createUser({});
+	describe("Entry created succesfully", async () => {
+		const { cookie } = await createUser({});
 
-        const res = await app.handle(
-            new Request(`${endpointPath}`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json; charset=utf-8",
-                    cookie: cookie,
-                },
-                body: JSON.stringify(EXAMPLE_DOCUMENT),
-            }),
-        );
+		const res = await app.handle(
+			new Request(`${endpointPath}`, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json; charset=utf-8",
+					cookie: cookie,
+				},
+				body: JSON.stringify(EXAMPLE_DOCUMENT),
+			}),
+		);
 
-        it("should return 201 status code", async () => {
-            expect(res.status).toBe(201);
-        });
+		it("should return 201 status code", async () => {
+			expect(res.status).toBe(201);
+		});
 
-        describe("return body", async () => {
-            const body: { id: string } = await res.json();
+		describe("return body", async () => {
+			const body: { id: string } = await res.json();
 
-            it("should return object on body response", () => {
-                expect(body).toBeObject();
-            });
+			it("should return object on body response", () => {
+				expect(body).toBeObject();
+			});
 
-            it("should return new entry id on body response", () => {
-                expect(body).toContainKey("id");
-                expect(body.id).toBeString();
-            });
-        });
-    });
+			it("should return new entry id on body response", () => {
+				expect(body).toContainKey("id");
+				expect(body.id).toBeString();
+			});
+		});
+	});
 
-    describe("Incorrect schema on body json", async () => {
-        const { cookie } = await createUser({});
+	describe("Incorrect schema on body json", async () => {
+		const { cookie } = await createUser({});
 
-        const res = await app.handle(
-            new Request(`${endpointPath}/`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json; charset=utf-8",
-                    cookie: cookie,
-                },
-                body: JSON.stringify({
-                    title: 1233,
-                    content: "asdfas",
-                    word_count: "asdf",
-                    iv: 123,
-                }),
-            }),
-        );
+		const res = await app.handle(
+			new Request(`${endpointPath}/`, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json; charset=utf-8",
+					cookie: cookie,
+				},
+				body: JSON.stringify({
+					title: 1233,
+					content: "asdfas",
+					word_count: "asdf",
+					iv: 123,
+				}),
+			}),
+		);
 
-        it("should return 400 status code", async () => {
-            expect(res.status).toBe(400);
-        });
+		it("should return 400 status code", async () => {
+			expect(res.status).toBe(400);
+		});
 
-        it("should return error message", async () => {
-            const body = await res.json();
+		it("should return error message", async () => {
+			const body = await res.json();
 
-            expect(body).toContainKey("error");
-        });
-    });
+			expect(body).toContainKey("error");
+		});
+	});
 });
--- apps/api/tests/lib/constants/index.ts
+++ apps/api/tests/lib/constants/index.ts
@@ -1,5 +1,6 @@
 import type { TInsertEntry } from "@/features/entry/models/entry.models";
 
 export const EXAMPLE_DOCUMENT: TInsertEntry = {
-    data: 'YSDEgCDwkICAIOaWhyDwn6aE', iv: 'YSDEgCDwkICAIOaWhyDwn6aE'
+	data: "YSDEgCDwkICAIOaWhyDwn6aE",
+	iv: "YSDEgCDwkICAIOaWhyDwn6aE",
 };
