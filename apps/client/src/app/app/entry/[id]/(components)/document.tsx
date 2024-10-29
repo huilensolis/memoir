4f6de4858c02d520d0c91789127fe83cdcb909a7
Github Action <huilensolis@users.noreply.github.com>
2024-10-29 15:43:31 +0000

    format(client): formatt code

--- apps/client/src/app/app/entry/[id]/(components)/document.tsx
+++ apps/client/src/app/app/entry/[id]/(components)/document.tsx
@@ -15,112 +15,112 @@ import { ClientRoutingService } from "@/models/routing/client";
 import { useRouter } from "next/navigation";
 
 export function Document({ entry }: { entry: TRawEntry }) {
-    const [decryptedData, setDecryptedData] = useState<TParsedEntry | null>(null);
-    const [clientDocumentData, setClientDocumentData] =
-        useState<TParsedEntry | null>(null);
+  const [decryptedData, setDecryptedData] = useState<TParsedEntry | null>(null);
+  const [clientDocumentData, setClientDocumentData] =
+    useState<TParsedEntry | null>(null);
 
-    const setEntryState = useEntryStore((state) => state.setState);
+  const setEntryState = useEntryStore((state) => state.setState);
 
-    const router = useRouter()
+  const router = useRouter();
 
-    useEffect(() => {
-        async function decryptData() {
-            try {
-                const cryptographyApi = new CryptographyCustomApi();
+  useEffect(() => {
+    async function decryptData() {
+      try {
+        const cryptographyApi = new CryptographyCustomApi();
 
-                const ivBuffer = Base64Parser.from_base64_to_arraybuffer(entry.iv);
-                const dataBuffer = Base64Parser.from_base64_to_arraybuffer(entry.data);
+        const ivBuffer = Base64Parser.from_base64_to_arraybuffer(entry.iv);
+        const dataBuffer = Base64Parser.from_base64_to_arraybuffer(entry.data);
 
-                const decryptedData = await cryptographyApi.decrypt({
-                    iv: ivBuffer,
-                    encryptedData: dataBuffer,
-                });
+        const decryptedData = await cryptographyApi.decrypt({
+          iv: ivBuffer,
+          encryptedData: dataBuffer,
+        });
 
-                const parsedDecryptedData: TParsedEntry = JSON.parse(decryptedData);
+        const parsedDecryptedData: TParsedEntry = JSON.parse(decryptedData);
 
-                setDecryptedData(parsedDecryptedData);
-                setClientDocumentData(parsedDecryptedData);
-            } catch (error) {
-                console.log({ error });
+        setDecryptedData(parsedDecryptedData);
+        setClientDocumentData(parsedDecryptedData);
+      } catch (error) {
+        console.log({ error });
 
-                setDecryptedData(null);
-                setClientDocumentData(null);
-            }
-        }
-
-        // eslint-disable-next-line @typescript-eslint/no-floating-promises
-        decryptData();
-    }, [entry]);
-
-    useEffect(() => {
-        async function updateDocument({ signal }: { signal: AbortSignal }) {
-            setEntryState("saving");
-
-            const updatedEntry: Partial<TParsedEntry> = {
-                ...clientDocumentData,
-            };
-
-            await EntryService.updateEntryById({
-                entryId: entry.id,
-                entry: updatedEntry,
-                signal,
-            });
-
-            setEntryState("up to date");
-
-            if (clientDocumentData?.title !== decryptedData?.title) {
-                await cleanCache(ClientRoutingService.app.home, "layout")
-                router.refresh()
-            }
-        }
-
-        const ctrl = new AbortController();
-
-        // eslint-disable-next-line @typescript-eslint/no-floating-promises
-        updateDocument({ signal: ctrl.signal });
-
-        return () => {
-            ctrl.abort();
-        };
-    }, [clientDocumentData, entry.id]);
-
-    function updateTitle(title: string) {
-        if (decryptedData === null) return;
-        if (clientDocumentData === null) return;
-
-        setClientDocumentData({ ...clientDocumentData, title });
+        setDecryptedData(null);
+        setClientDocumentData(null);
+      }
     }
 
-    function updateContent(content: TParsedEntry["content"]) {
-        if (decryptedData === null) return;
-        if (clientDocumentData === null) return;
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    decryptData();
+  }, [entry]);
 
-        setClientDocumentData({ ...clientDocumentData, content });
+  useEffect(() => {
+    async function updateDocument({ signal }: { signal: AbortSignal }) {
+      setEntryState("saving");
+
+      const updatedEntry: Partial<TParsedEntry> = {
+        ...clientDocumentData,
+      };
+
+      await EntryService.updateEntryById({
+        entryId: entry.id,
+        entry: updatedEntry,
+        signal,
+      });
+
+      setEntryState("up to date");
+
+      if (clientDocumentData?.title !== decryptedData?.title) {
+        await cleanCache(ClientRoutingService.app.home, "layout");
+        router.refresh();
+      }
     }
 
-    return (
-        <>
-            {decryptedData ? (
-                <div className="flex flex-col gap-3 w-full h-full">
-                    <header className="flex flex-col">
-                        <EntryTitle
-                            entryId={entry.id}
-                            defaultValue={decryptedData.title ?? ""}
-                            onUpdateTitle={updateTitle}
-                        />
-                    </header>
-                    <EntryEditor
-                        initialContent={decryptedData.content}
-                        entry={entry}
-                        onUpdate={updateContent}
-                    />
-                </div>
-            ) : (
-                <EntryPageLoading />
-            )}
-            <footer className="fixed bottom-0 lg:left-80 left-0 w-full">
-                <EntryOptions entryId={entry.id} />
-            </footer>
-        </>
-    );
+    const ctrl = new AbortController();
+
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    updateDocument({ signal: ctrl.signal });
+
+    return () => {
+      ctrl.abort();
+    };
+  }, [clientDocumentData, entry.id]);
+
+  function updateTitle(title: string) {
+    if (decryptedData === null) return;
+    if (clientDocumentData === null) return;
+
+    setClientDocumentData({ ...clientDocumentData, title });
+  }
+
+  function updateContent(content: TParsedEntry["content"]) {
+    if (decryptedData === null) return;
+    if (clientDocumentData === null) return;
+
+    setClientDocumentData({ ...clientDocumentData, content });
+  }
+
+  return (
+    <>
+      {decryptedData ? (
+        <div className="flex flex-col gap-3 w-full h-full">
+          <header className="flex flex-col">
+            <EntryTitle
+              entryId={entry.id}
+              defaultValue={decryptedData.title ?? ""}
+              onUpdateTitle={updateTitle}
+            />
+          </header>
+          <EntryEditor
+            initialContent={decryptedData.content}
+            entry={entry}
+            onUpdate={updateContent}
+          />
+        </div>
+      ) : (
+        <EntryPageLoading />
+      )}
+      <footer className="fixed bottom-0 lg:left-80 left-0 w-full">
+        <EntryOptions entryId={entry.id} />
+      </footer>
+    </>
+  );
 }
