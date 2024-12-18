6dc48422c8609908c214ab7bc06ce0e6008a7c62
Huilensolis <xywvanz@gmail.com>
2024-12-18 13:45:12 -0300

    feat(client): remove encryption key when singing out

--- apps/client/src/components/feature/auth/sign-out/sign-out.component.tsx
+++ apps/client/src/components/feature/auth/sign-out/sign-out.component.tsx
@@ -2,37 +2,57 @@
 
 import { Button } from "@/components/ui/button";
 import { AuthService } from "@/models/api/auth";
+import { CryptographyCustomApi } from "@/models/cryptography";
 import { ClientRoutingService } from "@/models/routing/client";
 import { LogOut } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 export function SignOutBtn() {
-  const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(false);
 
-  const router = useRouter();
+    const router = useRouter();
 
-  async function signOut() {
-    setLoading(true);
+    async function signOut() {
+        setLoading(true);
 
-    try {
-      await AuthService.signOut();
-      router.push(ClientRoutingService.auth.signIn);
-    } catch (error) {
-    } finally {
-      setLoading(false);
+        try {
+            const cryptoApi = new CryptographyCustomApi()
+
+            const doesClientHaveCryptoKey = await cryptoApi.doesClientHaveAStroredKey()
+
+            if (!doesClientHaveCryptoKey) {
+
+                await AuthService.signOut();
+                router.push(ClientRoutingService.auth.signIn);
+            }
+
+            const currentKey = cryptoApi.keyIdInDb
+
+            const { success } = await cryptoApi.removeKey(currentKey)
+
+            if (!success) throw new Error("could not remove key")
+
+            await AuthService.signOut();
+            router.push(ClientRoutingService.auth.signIn);
+
+
+        } catch (error) {
+            console.log({ error })
+        } finally {
+            setLoading(false);
+        }
     }
-  }
 
-  return (
-    <Button
-      variant="outline"
-      loading={loading}
-      onClick={signOut}
-      className="w-full flex gap-2"
-    >
-      <LogOut />
-      Sign Out
-    </Button>
-  );
+    return (
+        <Button
+            variant="outline"
+            loading={loading}
+            onClick={signOut}
+            className="w-full flex gap-2"
+        >
+            <LogOut />
+            Sign Out
+        </Button>
+    );
 }
