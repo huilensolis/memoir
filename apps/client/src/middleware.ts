d1dd4be7aac276dc8b14c7393ea8eabf63c4a825
Github Action <huilensolis@users.noreply.github.com>
2024-12-25 18:22:53 +0000

    format(client): formatt code

--- apps/client/src/middleware.ts
+++ apps/client/src/middleware.ts
@@ -6,41 +6,41 @@ import { AuthService } from "./models/api/auth";
 import { API_CONFIG } from "./config/api/api.config";
 
 export default async function middleware(request: NextRequest) {
-    const urlClone = request.nextUrl.clone();
-    urlClone.pathname = ClientRoutingService.auth.signIn;
-    const signInUrl = urlClone;
+  const urlClone = request.nextUrl.clone();
+  urlClone.pathname = ClientRoutingService.auth.signIn;
+  const signInUrl = urlClone;
 
-    const urlClone2 = request.nextUrl.clone()
-    urlClone2.pathname = "/"
-    const homeUrl = urlClone2
+  const urlClone2 = request.nextUrl.clone();
+  urlClone2.pathname = "/";
+  const homeUrl = urlClone2;
 
-    const cookieStore = cookies();
+  const cookieStore = cookies();
 
-    const accessToken = cookieStore.get(API_CONFIG.cookieName);
+  const accessToken = cookieStore.get(API_CONFIG.cookieName);
 
-    if (!accessToken?.value || !accessToken.name) {
-        return NextResponse.redirect(signInUrl);
+  if (!accessToken?.value || !accessToken.name) {
+    return NextResponse.redirect(signInUrl);
+  }
+
+  try {
+    const { isTokenValid, isServerUp } = await AuthService.checkToken({
+      cookies: accessToken.value,
+    });
+
+    if (!isServerUp) {
+      return NextResponse.redirect(homeUrl);
     }
 
-    try {
-        const { isTokenValid, isServerUp } = await AuthService.checkToken({
-            cookies: accessToken.value,
-        });
-
-        if (!isServerUp) {
-            return NextResponse.redirect(homeUrl);
-        }
-
-        if (!isTokenValid) {
-            const response = NextResponse.redirect(signInUrl)
-            response.cookies.delete(API_CONFIG.cookieName)
-            return response
-        }
-    } catch (error) {
-        return NextResponse.redirect(signInUrl);
+    if (!isTokenValid) {
+      const response = NextResponse.redirect(signInUrl);
+      response.cookies.delete(API_CONFIG.cookieName);
+      return response;
     }
+  } catch (error) {
+    return NextResponse.redirect(signInUrl);
+  }
 }
 
 export const config = {
-    matcher: ["/app/:path*"],
+  matcher: ["/app/:path*"],
 };
